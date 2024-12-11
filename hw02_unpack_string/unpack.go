30b1bc5c58386f0de4aa45a35ecfa9c92a389ed0
BashMS <maks100maks@gmail.com>
2024-12-11 08:09:08 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,75 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+// RtTrim обрезает последний символ строки.
+func RtTrim(sb *strings.Builder) error {
+	trimStr := sb.String()
+	sb.Reset()
+	_, err := sb.WriteString(string([]rune(trimStr)[0 : len([]rune(trimStr))-1]))
+	if err != nil {
+		return fmt.Errorf("%w WriteString", err)
+	}
+	return nil
+}
+
+// Unpack распаковывает строку.
+func Unpack(str string) (string, error) {
+	// Если пустая, сразу выходим
+	if len(str) == 0 {
+		return "", nil
+	}
+
+	// Пройдем по строке
+	var (
+		res    strings.Builder
+		prevRn rune
+	)
+	for i, item := range str {
+		// Если начали с цифры тогда сразу ошибка
+		switch {
+		case unicode.IsDigit(item) && i == 0:
+			return "", ErrInvalidString
+		case unicode.IsDigit(item) && unicode.IsDigit(prevRn):
+			return "", ErrInvalidString
+		case unicode.IsDigit(item):
+			cnt, err := strconv.Atoi(string(item))
+			if err != nil {
+				return "", fmt.Errorf("%w strconv.Atoi", err)
+			}
+			switch cnt {
+			case 1:
+				// Ничего не делаем, символ добавлен в предыдущей итерации
+				prevRn = item
+				continue
+			case 0:
+				// Уберем последний добавленный символ
+				err = RtTrim(&res)
+				if err != nil {
+					return "", fmt.Errorf("%w rtTrim", err)
+				}
+				prevRn = item
+				continue
+			default:
+				// Добавим символ n-1 раз
+				_, err = res.WriteString(strings.Repeat(string(prevRn), cnt-1))
+				if err != nil {
+					return "", fmt.Errorf("%w WriteString", err)
+				}
+				prevRn = item
+			}
+		default:
+			prevRn = item
+			res.WriteRune(item)
+		}
+	}
+
+	return res.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -2,6 +2,7 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -16,6 +17,10 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "a0b0c0", expected: ""},
+		{input: "буквв0а1", expected: "буква"},
+		{input: "буквыA1B2C3D4", expected: "буквыABBCCCDDDD"},
+
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
@@ -40,6 +45,36 @@ func TestUnpackInvalidString(t *testing.T) {
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
+		})
+	}
+}
+
+func TestRtTrim(t *testing.T) {
+	var (
+		in1  strings.Builder
+		in2  strings.Builder
+		out1 strings.Builder
+		out2 strings.Builder
+	)
+	in1.WriteString("aaaabccddddde")
+	out1.WriteString("aaaabccddddd")
+	in2.WriteString("a")
+	out2.WriteString("")
+
+	tests := []struct {
+		input    *strings.Builder
+		expected *strings.Builder
+	}{
+		{input: &in1, expected: &out1},
+		{input: &in2, expected: &out2},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.input.String(), func(t *testing.T) {
+			err := RtTrim(tc.input)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, tc.input)
 		})
 	}
 }
