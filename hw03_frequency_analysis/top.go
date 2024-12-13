7dbceb680a27eb0cbb0219d8a118d4642fc43753
BashMS <maks100maks@gmail.com>
2024-12-13 17:13:08 +0300

    hw03 разбивка слов и подсчет вхождений

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,22 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
+import "strings"
+
+func Top10(inText string) []string {
+	if len(inText) == 0 {
+		return nil
+	}
+
+	// Разобьем текст на слова
+	allFields := strings.Fields(inText)
+	listCnt := make(map[string]int, len(allFields))
+	for _, field := range allFields {
+		if cnt, ok := listCnt[field]; ok {
+			listCnt[field] = cnt + 1
+		} else {
+			listCnt[field] = 1
+		}
+	}
+
 	return nil
 }
