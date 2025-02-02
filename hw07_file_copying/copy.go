f9954dc9854e20e6c90edf02108f00c2e7dfd6d4
BashMS <maks100maks@gmail.com>
2025-02-02 16:21:51 +0300

    hw07 рабочая версия

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3" //nolint:depguard
 )
 
 var (
@@ -10,6 +15,59 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	// Получим размер копируемого файла
+	fileStt, err := os.Stat(fromPath)
+	if err != nil {
+		return fmt.Errorf("os.Stat: %w", err)
+	}
+	// Если указали отступ тогда проверим его валидность
+	if offset > fileStt.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+	if limit == 0 || limit > fileStt.Size() {
+		limit = fileStt.Size()
+	}
+
+	// Открываем файл
+	fileSrc, err := os.OpenFile(fromPath, os.O_RDONLY, 0o644)
+	if err != nil {
+		return fmt.Errorf("os.OpenFile: %w", err)
+	}
+	defer fileSrc.Close()
+	// устанавливаем смещение
+	if offset > 0 {
+		_, err = fileSrc.Seek(offset, 0)
+		if err != nil {
+			return fmt.Errorf("file.Seek: %w", err)
+		}
+	}
+	// Создаем файл приемник
+	fileDst, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("os.Create: %w", err)
+	}
+	defer fileDst.Close()
+
+	data := make([]byte, 1)
+
+	progressBar := pb.Start64(limit)
+	defer progressBar.Finish()
+	var curCnt int64
+	for {
+		_, err := fileSrc.Read(data)
+		if errors.Is(err, io.EOF) { // если конец файла
+			break // выходим из цикла
+		}
+		wb, err := fileDst.Write(data)
+		if err != nil {
+			return fmt.Errorf("fileDst.Write: %w", err)
+		}
+		curCnt += int64(wb)
+		progressBar.Add(wb)
+		if curCnt >= limit {
+			break
+		}
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,49 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	type args struct {
+		fromPath string
+		toPath   string
+		offset   int64
+		limit    int64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "case 01: offset exceeds file size",
+			args: args{
+				fromPath: "testdata/input.txt",
+				toPath:   "testdata/out.txt",
+				offset:   10000,
+				limit:    0,
+			},
+			wantErr: true,
+		},
+		{
+			name: "case 02: copy ok",
+			args: args{
+				fromPath: "testdata/input.txt",
+				toPath:   "testdata/out.txt",
+				offset:   0,
+				limit:    0,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := Copy(tt.args.fromPath, tt.args.toPath, tt.args.offset, tt.args.limit); (err != nil) != tt.wantErr {
+				t.Errorf("Copy() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			os.Remove(tt.args.toPath)
+		})
+	}
 }
