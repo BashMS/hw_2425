d211a85b6ec4670560d4ce1ece5dd3800ccc6348
BashMS <maks100maks@gmail.com>
2025-02-21 10:54:51 +0300

    hw10 рабочий вариант + правки линтера

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -7,52 +7,73 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/valyala/fastjson"
+	"github.com/valyala/fastjson" //nolint: depguard
 )
 
 type User struct {
-	ID       int    `json:"-"`
-	Name     string `json:"-"`
-	Username string `json:"-"`
-	Email    string `json:"Email"`
-	Phone    string `json:"-"`
-	Password string `json:"-"`
-	Address  string `json:"-"`
+	Email string
 }
 
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
+
 	u, err := getUsers(r)
+
 	if err != nil {
+
 		return nil, fmt.Errorf("get users error: %w", err)
+
 	}
+
 	return countDomains(u, domain)
+
 }
 
 type users []User
 
 func getUsers(r io.Reader) (result users, err error) {
+
 	scanner := bufio.NewScanner(r)
+
 	for scanner.Scan() {
+
 		var user User
+
 		user.Email = fastjson.GetString(scanner.Bytes(), "Email")
-	 	result = append(result, user)
+
+		result = append(result, user)
+
 	}
+
 	if err := scanner.Err(); err != nil {
+
 		return nil, err
+
 	}
+
 	return result, nil
+
 }
 
 func countDomains(u users, domain string) (DomainStat, error) {
+
 	result := make(DomainStat)
+
 	rg := regexp.MustCompile("\\." + domain)
+
 	for _, user := range u {
+
 		matched := rg.MatchString(user.Email)
+
 		if matched {
+
 			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]++
+
 		}
+
 	}
+
 	return result, nil
+
 }
