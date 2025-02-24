a2a156ba1b9ab80446ae721978ad2ae8df15f8f2
BashMS <maks100maks@gmail.com>
2025-02-24 16:43:38 +0300

    hw06 не работает

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -13,49 +13,41 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	//ress := make([]Out, 0)
-	result := make(Out)
+	result := make(Bi)
+	defer close(result)
 	wg := sync.WaitGroup{}
-
-	select {
-	case <-done:
-		return nil
-	default:
-		for i, stg := range stages {
-			//result := make(Out)
-			//result = stages[0](in)
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				select {
-				case <-done:
-					return
-				default:
-					if i > 0 {
-						result = stg(result)
-					} else {
-						result = stg(in)
-					}
+	for val := range in {
+		chIn := make(Bi,1)
+		chIn <- val
+		close(chIn)
+		wg.Add(1)
+		go func(ch In) {
+			defer wg.Done()
+			select {
+			case <-done:
+			   return
+			default:
+			   chOut := make(Out)
+		       for i, stg := range stages {
+			        select {
+			        case <-done:
+			            return 
+		            default:
+			            if i > 0 {
+					      chOut = stg(chOut)
+			            } else {
+					      chOut = stg(ch)
+			            }
+			        }
+			    }
+				for val := range chOut {
+					result <- val
 				}
-			}()
-
-		}
-		//ress = append(ress, result)
+		    }
+	    }(chIn)
 	}
 
 	wg.Wait()
-	select {
-	case <-done:
-		return nil
-	default:
-		//results := make(chan interface{}, len(ress))
-		//for _, item := range ress {
-		//	results <- fmt.Sprintf("%v", <-item)
-		//}
-		//close(results)
-		//return results
-		return result
-	}
-
-	//return result
+	
+	return result
 }
