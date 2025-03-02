276090c65707a333d96f5692201f503de7c504e0
BashMS <maks100maks@gmail.com>
2025-03-02 13:21:39 +0300

    hw11 первое приблежение

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,99 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	timeout := flag.Duration("timeout", 10*time.Second, "timeout for connection")
+
+	flag.Parse()
+
+	args := flag.Args()
+
+	if len(args) != 2 {
+		fmt.Fprintln(os.Stderr, "Usage: go-telnet --timeout=10s host port")
+
+		os.Exit(1)
+	}
+
+	address := net.JoinHostPort(args[0], args[1])
+
+	client := NewTelnetClient(address, *timeout, os.Stdin, os.Stdout)
+
+	if err := client.Connect(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+
+		os.Exit(1)
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGKILL)
+
+	defer cancel()
+
+	ctx, stop := context.WithTimeout(ctx, *timeout)
+
+	defer stop()
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+
+		writeRoutine(ctx, client)
+	}()
+
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+
+		readRoutine(ctx, client)
+	}()
+
+	wg.Wait()
+
+	client.Close()
+}
+
+func readRoutine(ctx context.Context, client TelnetClient) {
+	for {
+		select {
+		case <-ctx.Done():
+
+			return
+		default:
+
+			err := client.Receive()
+			if err != nil {
+				ctx.Done()
+			}
+		}
+	}
+}
+
+func writeRoutine(ctx context.Context, client TelnetClient) {
+	for {
+		select {
+		case <-ctx.Done():
+
+			return
+		default:
+
+			err := client.Send()
+			if err != nil {
+				ctx.Done()
+			}
+		}
+	}
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,21 +1,101 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
 type TelnetClient interface {
 	Connect() error
+
 	io.Closer
+
 	Send() error
+
 	Receive() error
 }
 
+// TClient - структура для хранения состояния клиента + имплиментируем методы основного интерфейса
+
+// По другому не придумал как адрес, таймаут и коннект передавть между методами.
+
+type TClient struct {
+	ctx context.Context
+
+	in *io.ReadCloser
+
+	out *io.Writer
+
+	address string
+
+	timeout time.Duration
+
+	conn *net.Conn
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	// Создаем контекст с таймаутом и отменой
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+
+	defer cancel()
+
+	t := TClient{
+		ctx: ctx,
+
+		in: &in,
+
+		out: &out,
+
+		address: address,
+
+		timeout: timeout,
+	}
+
+	return &t
+}
+
+func (t *TClient) Connect() error {
+	dialer := &net.Dialer{}
+
+	conn, err := dialer.DialContext(t.ctx, "tcp", t.address)
+	if err != nil {
+		return fmt.Errorf("dialer.DialContext: %w", err)
+	}
+
+	defer conn.Close()
+
+	t.conn = &conn
+
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (t *TClient) Close() error {
+	err := (*t.conn).Close()
+	if err != nil {
+		return fmt.Errorf("conn.Close: %w", err)
+	}
+
+	return nil
+}
+
+func (t *TClient) Send() error {
+	_, err := io.Copy(*t.conn, *t.in)
+	if err != nil {
+		return fmt.Errorf("io.Copy: %w", err)
+	}
+
+	return nil
+}
+
+func (t *TClient) Receive() error {
+	_, err := io.Copy(*t.out, *t.conn)
+	if err != nil {
+		return fmt.Errorf("io.Copy: %w", err)
+	}
+
+	return nil
+}
